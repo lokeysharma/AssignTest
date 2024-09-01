6bb7902d41aa3ac42211a2057d1079317e65c06b
Lokesh Sharma <Lokesh.sharma@hatch.com>
2024-09-01 19:10:07 +0530

    Project initialization

---
+++ src/main/java/com/lokey/learn/assigntest/AssignTestApplication.java
@@ -0,0 +1,13 @@
+package com.lokey.learn.assigntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lokey/learn/assigntest/AssignTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokey.learn.assigntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
